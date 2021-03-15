c2af90af5e066b0eb478e148fc9dcf3659806173
unknown <kevin.cheung.kai@gmail.com>
2021-03-15 16:59:59 -0400

    implemented ball collision with wall and paddles

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +11,9 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+ball.movement()
 
 screen.listen()
 screen.onkey(r_paddle.up, 'Up')
@@ -16,12 +21,19 @@ screen.onkey(r_paddle.down, 'Down')
 screen.onkey(l_paddle.up, 'w')
 screen.onkey(l_paddle.down, 's')
 
-r_paddle.pong_paddle()
-l_paddle.pong_paddle()
-
 game_is_on = True
 while game_is_on:
+    time.sleep(0.2)
     screen.update()
+    ball.movement()
 
+    # Detect collision with top wall
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
+
